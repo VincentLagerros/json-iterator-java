0bc0e7c693ba98f044570bc6c6bb0c422d1890ee
ErikBetzholtz <erik.betzholtz@gmail.com>
2025-02-20 21:55:17 +0100

    test: add tests to readStringSlowPath #8
    will add three tests for increased code coverage in readStringSlowPath. Will increase code coverage to 15/44 branches covered

--- src/main/java/com/jsoniter/IterImpl.java
+++ src/main/java/com/jsoniter/IterImpl.java
@@ -321,7 +321,7 @@ class IterImpl {
                             break;
 
                         default:
-                        setBanchEntry(23);// Id 23
+                            setBanchEntry(23);// Id 23
                             throw iter.reportError("readStringSlowPath", "invalid escape character: " + bc);
                     }
 
@@ -391,9 +391,10 @@ class IterImpl {
                 setBanchEntry(41);// Id 41
                 iter.reusableChars[j++] = (char) bc;
             }
+            setBanchEntry(42);// Id 42
             throw iter.reportError("readStringSlowPath", "incomplete string");
         } catch (IndexOutOfBoundsException e) {
-            setBanchEntry(42);// Id 42
+            setBanchEntry(43);// Id 43
             throw iter.reportError("readString", "incomplete string");
         }
     }
--- src/test/java/com/jsoniter/IterImplTest.java
+++ src/test/java/com/jsoniter/IterImplTest.java
@@ -1,5 +1,9 @@
 package com.jsoniter;
 
+import java.io.IOException;
+
+import com.jsoniter.spi.JsonException;
+
 import junit.framework.TestCase;
 
 public class IterImplTest extends TestCase{
@@ -11,9 +15,36 @@ public class IterImplTest extends TestCase{
 		String res = new String(iter.reusableChars, 0, endPos);
 		IterImpl.writeCCToFile();
 		assertEquals(sample, res);
-
-		
 	}
 
+	public void testReadEscapedCharString() throws Exception {
+		String sample = "\b\t\n\f\r/\\";
+		JsonIterator iter = JsonIterator.parse("\\b\\t\\n\\f\\r\\/\\\\\"");
+		int endPos = IterImpl.readStringSlowPath(iter, 0);
+		String res = new String(iter.reusableChars, 0, endPos);
+		IterImpl.writeCCToFile();
+		assertEquals(sample, res);
+	}
 
+	public void testDynamicBufResize() throws Exception {
+		String sample = "longStringLongStringLongStringLongStringLongStringLongStringLongStringLongString";
+		JsonIterator iter = JsonIterator.parse("longStringLongStringLongStringLongStringLongStringLongStringLongStringLongString\"");
+		int endPos = IterImpl.readStringSlowPath(iter, 0);
+		String res = new String(iter.reusableChars, 0, endPos);
+		IterImpl.writeCCToFile();
+		assertEquals(sample, res);
+	}
+
+	public void testIncompleteString() throws Exception {
+		JsonIterator iter = JsonIterator.parse("incomplete");
+		boolean flag = true;
+		try{
+			IterImpl.readStringSlowPath(iter, 0);
+		}catch(Exception e){
+			assertTrue(e.getMessage().contains("incomplete string"));
+			flag = false;
+		}
+		if(flag) 
+			fail();
+	}
 }
